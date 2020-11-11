30b9d553cb98d6a80d51b2057a9a38fc1d88d0ca
kbc1235 <pok_hu111@nate.com>
2020-11-11 09:32:17 +0900

    10일 커밋

--- airbnb/src/layout/visual.js
+++ airbnb/src/layout/visual.js
@@ -4,7 +4,11 @@ import React from 'react';
 function Visual(){
     return (
        <div className="visual_wrapper">
-           <img src="https://a0.muscache.com/im/pictures/8203f5ab-f1a9-4574-b97c-b7ad693d83fb.jpg?im_q=highq&im_w=720"></img>
+           <div className="text_box">
+               <p>이제, 여행은<br/>가까운곳에서</p>
+               <button>근처의 숙소 둘러보기</button>
+           </div>
+           <img src="https://a0.muscache.com/im/pictures/de34af12-d42b-4857-9ec4-65d240db6fc2.jpg"></img>
        </div>
     );
 }
